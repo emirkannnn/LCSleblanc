960ed445956a541a50be362e5fee59502d1a0e50
emirkan1 <birisi1kaan@hotmail.com>
2016-04-06 16:36:19 +0300

    LCS
    LCS

---
+++ Slide.cs
@@ -0,0 +1,13 @@
+﻿using EloBuddy;
+using SharpDX;
+
+namespace Leblanc
+{
+    internal class Slide
+    {
+        public float ExpireTime { get; internal set; }
+        public int NetworkId { get; internal set; }
+        public GameObject Object { get; internal set; }
+        public Vector3 Position { get; internal set; }
+    }
+}
